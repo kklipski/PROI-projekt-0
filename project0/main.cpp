761fe90b58ed9d1dac80cee7ce43d33846e5a077
klipski <klipski@mion.elka.pw.edu.pl>
2017-03-09 03:45:38 +0100

    Pierwszy commit

---
+++ project0/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name, surname, pseudo;
+    cout<<"Write name:"<<endl;
+    cin>>name;
+    cout<<"Write surname:"<<endl;
+    cin>>surname;
+    cout<<"Write pseudonym:"<<endl;
+    cin>>pseudo;
+    cout<<name<<" "<<"\""<<pseudo<<"\""<<" "<<surname<<endl;
+    return 0;
+}
+}
